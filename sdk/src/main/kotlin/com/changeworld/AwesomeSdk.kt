a842b8d276cb3fb4dae3a2448e1258bf21368b3d
Giorgi Gusakov <giorgi.gusakov@s44.team>
2024-02-21 23:52:16 +0100

    5. Better error message

--- sdk/src/main/kotlin/com/changeworld/AwesomeSdk.kt
+++ sdk/src/main/kotlin/com/changeworld/AwesomeSdk.kt
@@ -4,6 +4,7 @@ import org.koin.dsl.module
 
 object AwesomeSdk {
 
+    private var initialized = false
     fun init(awesomeSdkLogger: AwesomeSdkLogger) {
         val dynamicModule = module {
             single {
@@ -11,9 +12,13 @@ object AwesomeSdk {
             }
         }
         IsolatedKoinContext.koinApp.koin.loadModules(listOf(dynamicModule))
+        initialized = true
     }
 
     fun getCredentialsManager(): CredentialsManager {
+        if (!initialized) {
+            throw IllegalStateException("AwesomeSdk is not initialized. Please call AwesomeSdk.init() first.")
+        }
         return IsolatedKoinContext.koin.get()
     }
 }
